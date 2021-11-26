318224461987d338497ecac32cc36f7d5cd11ffe
Sujyot <70472979+SujyotRaut@users.noreply.github.com>
2021-11-26 18:02:47 +0530

    First commit

---
+++ prisma/seed.ts
@@ -0,0 +1,146 @@
+import { PrismaClient } from '@prisma/client';
+import fs from 'fs';
+import path from 'path';
+
+const prisma = new PrismaClient();
+
+interface ReviewJSON {
+  reviewer_name: string;
+  review_rating: number;
+  review_date: string;
+  review: string;
+}
+
+interface ProductJSON {
+  id: string;
+  name: string;
+  brand: string;
+  sizes: Array<string>;
+  colors: Array<string>;
+  images: Array<string>;
+  rating: number;
+  rating_count: number;
+  reviews: Array<ReviewJSON>;
+  category: string;
+  discount: number;
+  original_price: number;
+  discounted_price: number;
+  tags: Array<string>;
+}
+
+async function main() {
+  const data: Array<ProductJSON> = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, 'data.json'), 'utf-8')
+  );
+
+  const products = data.map(mapFunc);
+
+  for (const product of products) {
+    await createProduct(product);
+    for (const review of product.reviews) {
+      await createReview(review, product.id);
+    }
+  }
+
+  console.log(
+    `${await prisma.product.count()}/${products.length} Products created`
+  );
+}
+
+async function createProduct(product: ProductJSON) {
+  await prisma.product.upsert({
+    where: { productId: product.id },
+    update: {},
+    create: {
+      productId: product.id,
+      name: product.name,
+      brand: product.brand,
+      category: product.category,
+      images: JSON.stringify(product.images),
+      discount: product.discount,
+      price: product.original_price,
+      rating: product.rating,
+      ratingCount: product.rating_count,
+      colors: {
+        connectOrCreate: product.colors.map((color) => ({
+          where: { color },
+          create: { color },
+        })),
+      },
+      sizes: {
+        connectOrCreate: product.sizes.map((size) => ({
+          where: { size },
+          create: { size },
+        })),
+      },
+      tags: {
+        connectOrCreate: product.sizes.map((tag) => ({
+          where: { tag },
+          create: { tag },
+        })),
+      },
+    },
+  });
+}
+
+async function createReview(review: ReviewJSON, productId: string) {
+  const email = `${review.reviewer_name.replace(' ', '.')}@gmail.com`;
+  await prisma.review.create({
+    data: {
+      rating: review.review_rating,
+      review: review.review,
+      reviewer: {
+        connectOrCreate: {
+          where: { email },
+          create: {
+            name: review.reviewer_name,
+            email,
+          },
+        },
+      },
+      product: { connect: { productId } },
+    },
+  });
+}
+
+// Convert product fields to appropriate types
+function mapFunc(product: ProductJSON) {
+  // Convert rating string to number
+  product.rating = parseFloat(`${product.rating}`);
+
+  // Convert rating_count string to number
+  const temp = `${product.rating_count}`.split(' ')[0];
+  const count = temp.endsWith('k') ? parseFloat(temp) * 1000 : parseInt(temp);
+  product.rating_count = count;
+
+  // Convert discount string to number
+  const discount = parseInt(`${product.discount}`.replace('(', ''));
+  product.discount = discount;
+
+  // Convert original_price string to number
+  const original_price = parseInt(`${product.original_price}`.split(' ')[1]);
+  product.original_price = original_price;
+
+  // Convert discounted_price string to number
+  const discounted_price = parseInt(
+    `${product.discounted_price}`.split(' ')[1]
+  );
+  product.discounted_price = discounted_price;
+
+  // Convert review_rating string to number
+  product.reviews = product.reviews.map((review) => {
+    review.review_rating = parseFloat(`${review.review_rating}`);
+    return review;
+  });
+
+  return product;
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
